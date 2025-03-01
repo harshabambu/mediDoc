f6472337f8f0d3db5fe92530404e5350a3a01c7d
harsha12a <harsha.pss2005@gmail.com>
2025-03-01 10:37:06 +0530

    add:logo

--- frontend/src/RootLayout.jsx
+++ frontend/src/RootLayout.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import {Outlet} from 'react-router-dom'
+import bot from './assets/bot1.png'
 function RootLayout() {
   return (
     <div>
         <Header />
         <div className='min-h-screen'>
           <Outlet />
+          <div className='fixed bottom-10 right-10 z-50 border bg-gray-300 border-gray-300 p-2 rounded-full'>
+            <img src={bot} alt='bot' className='w-12 h-12' />
+          </div>
         </div>
         <Footer />
     </div>
